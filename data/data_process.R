d223bb88510aa00b447dd338b64513275f406448
Leonardo Amadori <leonardo.amadori4@studio.unibo.it>
2026-04-30 10:21:43 +0200

    updated the data_process.R file to allow it to put the variables it finds in seseparate vectors

--- data/data_process.R
+++ data/data_process.R
@@ -1,6 +1,17 @@
-# Check for .csv data in the folders, read the data
+# check for .csv data in the folders, read the data
 
+# searches for .csv files in the working directory
+list_csv <- list.files(pattern = "\\.csv$", recursive = TRUE) 
 
-list_csv <- list.files(pattern = "\\.csv$", recursive = TRUE) # searches for .csv files in the working directory
-clean_list_csv <- basename(list_csv)                          # cleans the file names and put them in a vector
-print(clean_list_csv)                                         # prints the cleaned names
+# cleans the file names and put them in a vector
+clean_list_csv <- basename(list_csv)                          
+
+# prints the cleaned names
+# print(clean_list_csv)                                         
+
+# extracts the first three letters from the name of each file
+prefixes <- substr(sub("\\.csv$", "", clean_list_csv), 1, 4)
+
+# creates a list of vectors and makes them single variables
+csv_groups <- split(clean_list_csv, prefixes)
+list2env(csv_groups, envir = .GlobalEnv)
