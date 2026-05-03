1db911d4978355b3b5725465f9ab2537c40850ee
Leonardo Amadori <leonardo.amadori4@studio.unibo.it>
2026-05-03 10:07:45 +0200

    fixed an issue for the scripts that tests biases_parameters_examples.R, the test was not able to find the correct location of the datas it was testing (which led to a test fail

--- test/test-biases_parameters_examples.R
+++ test/test-biases_parameters_examples.R
@@ -1,9 +1,24 @@
+# here: builds file paths relative to the project root
+# https://cran.r-project.org/web/packages/here/index.html
+library(here)
+
+# loads the main script to make its variables available in the test environment
+source(here::here("biases_parameters_examples", "biases_parameters_examples.R"))
+
 # testthat: allows to write tests for the other scripts
 # https://cran.r-project.org/web/packages/testthat/index.html
 library(testthat)
 
+test_that("The object isn't exactly what you were looking for...",{
 # test the dimension of the correlation matrixes
 expect_shape(cor_D5, dim = c(5, 5))
 expect_shape(cor_D30, dim = c(30, 30))
 expect_shape(cor_D100, dim = c(100, 100))
+
+# test dimension of the ToyModels
+expect_length(toy_D5_P100, 9)
+expect_length(toy_D5_P50, 9)
+expect_length(toy_D30_P50, 9)
+
+})
 
