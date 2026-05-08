e2b053c22c6f9e82760e874994b6eba651740462
Leonardo Amadori <leonardo.amadori4@studio.unibo.it>
2026-05-08 09:47:57 +0200

    minor fixes on test-01_plots_compositional_effects.R

--- test/test-01_plots_compositional_effects.R
+++ test/test-01_plots_compositional_effects.R
@@ -16,9 +16,29 @@ test_that("The file is not in the folder", {
     full.names = TRUE
   )
   
-  # check that found paths match the expected one
+  # check that found path match the expected one
   expect_setequal(found_data_frame, expected_data_frame)
 })
 
-# --
+# -- test correct column name and size --------
 
+test_that("there is a problem with the file columns: either one is empty or doesn't exist!", {
+  # read the file
+  df <- readRDS(here("compositional_effects", "compositional_effects_02.rds"))
+  
+  # required columns must be present
+  expect_true(all(c("d", "pielou", "ERR_L1", "ERR_CLR") %in% names(df)))
+  
+  # must not be empty
+  expect_gt(nrow(df), 0)
+})
+
+# -- check the data frame after the pipelines --------
+
+test_that("there is a problem with the pipelined dataframe!",{
+  # read the file
+  df <- readRDS(here("compositional_effects", "compositional_effects_02.rds"))
+  
+  expect_true(nrow(df) == 1560)
+  
+})
