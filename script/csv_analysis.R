824f5925b3801b99133c5ff129fcac921d196ef7
Leonardo Amadori <leonardo.amadori4@studio.unibo.it>
2026-05-26 09:47:21 +0200

    fixed the comments in the csv_analysis.R script

--- script/csv_analysis.R
+++ script/csv_analysis.R
@@ -6,14 +6,21 @@ library(here)
 
 analysis <- function(x){
   
-  cat("column number is:", ncol(x),      "\n\n") # get column number
-  cat("column names are:", colnames(x),  "\n\n") # get column names
+  # get column number
+  cat("column number is:", ncol(x),      "\n\n") 
+  # get column names
+  cat("column names are:", colnames(x),  "\n\n") 
   
-  cat("row number is:"    , nrow(x),     "\n\n") # get row number
-  if (nrow(x) < 10) { # print row names only if number of rows is < 10
-  cat("row names are:"    , rownames(x), "\n\n") # get row names  
+  # get row number
+  cat("row number is:"    , nrow(x),     "\n\n") 
+  # print row names only if number of rows is < 10
+  if (nrow(x) < 10) { 
+  # get row names  
+  cat("row names are:"    , rownames(x), "\n\n")   
   }
-  cat("number of vectors in the file is:",  length(x), "\n") # gets number of vectors
+  
+  # gets number of vectors
+  cat("number of vectors in the file is:",  length(x), "\n") 
   # head(x)
 }
 
